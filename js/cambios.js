52c39c85b7039c7a1b65d70ad254ca9f5b5331e5
Stefy <stefydiazc@gmail.com>
2017-06-06 12:28:47 -0500

    Taller

---
+++ js/cambios.js
@@ -0,0 +1,23 @@
+function resaltar(){
+    var text = document.getElementById("a1").innerHTML;
+    console.log(text);
+    
+    
+    /*if (text == "") { alert("ERROR"); return false; }    
+    else {
+    var parrafo = new RegExp("(\\b" + text + "\\b)", "gim");
+    var anterior = document.getElementById("searchtext").innerHTML;
+    var p = anterior.replace(/(<span>|<\/span>)/igm, "");
+    document.getElementById("searchtext").innerHTML = p;
+    var x = p.replace(parrafo, "<span>$1</span>");
+    document.getElementById("searchtext").innerHTML = x;*/
+    
+    if(text )
+    {
+        
+    }else{
+        
+        
+    }
+    
+}
